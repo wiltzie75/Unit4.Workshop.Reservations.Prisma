e35785c1e3ab7e384eb10607a4adf3f5046729e0
Mark Lawrence <mark.lawrence@fullstackacademy.com>
2024-09-06 13:13:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -77,8 +77,11 @@ app.delete(
 );
 
 // Simple error handling middleware
-app.use((error, req, res, next) => {
-  res.status(res.status || 500).send({ error: error });
+app.use((err, req, res, next) => {
+  console.error(err);
+  const status = err.status ?? 500;
+  const message = err.message ?? 'Internal server error.';
+  res.status(status).json({ message });
 });
 
 app.listen(PORT, () => {
